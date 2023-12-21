b4d070208aa85b3aca82a73ec0b75063a28f38e3
KACPER SINILO <kacperosinilo@gmail.com>
2023-12-21 12:03:53 +0100

    fix(build): types errors

--- app/(routes)/blocked/page.tsx
+++ app/(routes)/blocked/page.tsx
@@ -1,34 +1,21 @@
-"use client"
-
-import { useEffect } from "react"
 import Image from "next/image"
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 import error_img from "@/public/error.webp"
 
-import { Button } from "@/components/ui/button"
-
-export default function Error({
-    error,
-    reset,
-}: {
-    error: Error & { digest?: string }
-    reset: () => void
-}) {
-    useEffect(() => {
-        console.error(error)
-    }, [error])
-
-    const router = useRouter()
-
+export default function BlockedPage() {
     return (
         <div className="flex w-full flex-col items-center justify-center space-y-4 pt-20">
-            <h2 className="text-center text-7xl font-bold uppercase text-blue-700">
+            <h2 className="text-center text-7xl font-bold uppercase text-green-500">
                 Something went wrong!
             </h2>
 
             <Image src={error_img} width={600} height={600} alt="error 404" />
 
-            <Button onClick={() => router.push("/")}>Try again</Button>
+            <Link href="/">
+                <p className="rounded-md border border-green-300 bg-green-300 px-8 py-3 text-white transition duration-200 hover:bg-white hover:text-green-300">
+                    Return Home
+                </p>
+            </Link>
         </div>
     )
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import not_found_img from "@/public/404.webp"
 export default function NotFound() {
     return (
         <div className="flex w-full flex-col items-center justify-center space-y-4 pt-20">
-            <h2 className="text-7xl font-bold uppercase text-blue-700">
+            <h2 className="text-7xl font-bold uppercase text-green-700">
                 Not Found
             </h2>
             <p className=" text-muted-foreground">
@@ -18,7 +18,7 @@ export default function NotFound() {
                 alt="error 404"
             />
             <Link href="/">
-                <p className="rounded-md border border-blue-300 bg-blue-300 px-8 py-3 text-white transition duration-200 hover:bg-white hover:text-blue-300">
+                <p className="rounded-md border border-green-300 bg-green-300 px-8 py-3 text-white transition duration-200 hover:bg-white hover:text-green-300">
                     Return Home
                 </p>
             </Link>
